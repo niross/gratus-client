91cf38a4319aaca4f41e1dc62b340dd7184652a2
Nick Ross <nick.ir.ross@gmail.com>
2016-06-15 16:58:51 +0100

    Wrap posts in a provider and update store with user data

--- app/Gratus.js
+++ app/Gratus.js
@@ -1,15 +1,11 @@
 import React, { PropTypes } from 'react';
-import {
-  StyleSheet,
-  Text,
-  View,
-  StatusBar,
-  Navigator,
-  AsyncStorage
-} from 'react-native';
+import { Provider } from 'react-redux';
 
 import { Authenticator } from 'jwt-authenticator';
 import { host } from './config';
+import { PostsContainer } from './containers/Posts';
+import { configureStore } from './store';
+import { setUser } from './actions/user';
 
 const propTypes = {
   navigator: PropTypes.object,
@@ -17,48 +13,26 @@ const propTypes = {
 };
 const defaultProps = {};
 
-export class Gratus extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+const store = configureStore();
 
-  render() {
-    return (
-      <Authenticator
-        navigator={this.props.navigator}
-        route={this.props.route}
-        authenticateEndpoint={`${host}/api/accounts/authenticate`}
-        registerEndpoint={`${host}/api/accounts/register`}
-        logoText="Gratus"
-      >
-        <View style={styles.container}>
-          <Text style={styles.welcome}>
-            Welcome to Gratus!
-          </Text>
-        </View>
-      </Authenticator>
-    );
-  }
-}
+export const Gratus = ({ navigator, route }) =>
+  <Authenticator
+    navigator={navigator}
+    route={route}
+    authenticateEndpoint={`${host}/api/accounts/authenticate`}
+    registerEndpoint={`${host}/api/accounts/register`}
+    logoText="Gratus"
+    onAuthenticate={(user) => {
+      store.dispatch(setUser(user));
+    }}
+  >
+    <Provider store={store}>
+      <PostsContainer
+        navigator={navigator}
+        route={route}
+      />
+    </Provider>
+  </Authenticator>;
 
 Gratus.propTypes = propTypes;
 Gratus.defaultProps = defaultProps;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF'
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5
-  }
-});
--- app/store.js
+++ app/store.js
@@ -1,16 +1,16 @@
-import { Map } from 'immutable';
+import { List, Map } from 'immutable';
 import thunkMiddleware from 'redux-thunk';
 import { createStore, applyMiddleware, combineReducers } from 'redux';
 
 import { reducer as posts } from './reducers/posts';
-import { reducer as users } from './reducers/user';
+import { reducer as user } from './reducers/user';
 
-const reducer = combineReducers({ users, posts });
+const reducer = combineReducers({ user, posts });
 
 export function configureStore() {
   return createStore(
     reducer,
-    Map(),
+    { posts: List(), user: Map() },
     applyMiddleware(
       thunkMiddleware
     )
